e5a5eaf1120c66fc73c6a7c7fc53fb7abe531eb4
Grisha <grisha-mateys@yandex.ru>
2020-12-04 23:25:31 +0700

    first test commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//alert("121");
+//console.log("123");
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt ("Вам есть 18?","18");
+//console.log(answer + 5);
+
+//const answers = [];
+
+//answers [0] = prompt('What is your name?', '');
+//answers [1] = prompt('What is your surname?', '');
+//answers [2] = prompt('How old are you?', '');
+
+//console.log(typeof(answers));
+
+const user = "Griha";
+ alert(`Hello, ${user}`);
